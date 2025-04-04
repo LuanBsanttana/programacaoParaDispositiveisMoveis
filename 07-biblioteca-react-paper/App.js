f41d82b150d722e8ccc6c04f963b706d206226fb
LuanBsanttana <luan.santana@iesb.edu.br>
2025-04-03 21:28:45 -0300

    Exercicio lista de times

--- 07-biblioteca-react-paper/App.js
+++ 07-biblioteca-react-paper/App.js
@@ -1,49 +1,116 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, View, ScrollView, FlatList} from "react-native";
 import { PaperProvider, Card, Title, Paragraph, Text, Button, Divider} from "react-native-paper";
+import Estados from "./components/Estados";
+
+
 
 
 export default function App() {
-
-  const listaFrutas = ["Uva", "Maça", "Laranja", "Melão"]
+  const listaEstadosMunicipios = [
+    {
+      nome: 'Rio de Janeiro',
+      sigla: 'RJ',
+      imagem: 'https://picsum.photos/700',
+      descricao: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+      municipios: [
+        {
+          nome: 'Rio de Janeiro',
+          imagem: 'https://picsum.photos/700'
+        },
+        {
+          nome: 'Niterói',
+          imagem: 'https://picsum.photos/700',
+        },
+        {
+          nome: 'Petrópolis',
+          imagem: 'https://picsum.photos/70',
+        },
+        {
+          nome: 'Angra dos Reis',
+          imagem: 'https://picsum.photos/700,'
+        },
+        {
+          nome: 'Cabo Frio',
+          imagem: 'https://picsum.photos/700',
+        }
+      ]
+    },
+    {
+      nome: 'São Paulo',
+      sigla: 'SP',
+      imagem: 'https://picsum.photos/700',
+      descricao: 'São Paulo é o estado mais populoso do Brasil, com uma economia diversificada e forte.',
+      municipios: [
+        {
+          nome: 'São Paulo',
+          imagem: 'https://picsum.photos/700'
+        },
+        {
+          nome: 'Campinas',
+          imagem: 'https://picsum.photos/700',
+        },
+        {
+          nome: 'Santos',
+          imagem: 'https://picsum.photos/700',
+        },
+        {
+          nome: 'Sorocaba',
+          imagem: 'https://picsum.photos/700',
+        },
+        {
+          nome: 'Ribeirão Preto',
+          imagem: 'https://picsum.photos/700',
+        }
+      ]
+    },
+    {
+      nome: 'Minas Gerais',
+      sigla: 'MG',
+      imagem: 'https://picsum.photos/700',
+      descricao: 'Minas Gerais é conhecido por sua rica história, culinária e belas paisagens.',
+      municipios: [
+        {
+          nome: 'Belo Horizonte',
+          imagem: 'https://picsum.photos/700',
+        },
+        {
+          nome: 'Ouro Preto',
+          imagem: 'https://picsum.photos/700',
+        },
+        {
+          nome: 'Uberlândia',
+          imagem: 'https://picsum.photos/700',
+        },
+        {
+          nome: 'Juiz de Fora',
+          imagem: 'https://picsum.photos/700',
+        },
+        {
+          nome: 'Montes Claros',
+          imagem: 'https://picsum.photos/700',
+        }
+      ]
+    }
+  ]
+  
   return (
     <PaperProvider>
-      <ScrollView>
       <View style={styles.container}>
-        <StatusBar style="auto" />
-        {
-          listaFrutas.map(
-            fruta => <Text variant="displaySmall">{fruta}</Text>
-          )
-        }
-        {
-          listaFrutas.map(
-            fruta=>(
-              <View>
-                <Text variant="displaySmall">{fruta}</Text>
-              
-              </View>  
-            )
-          )
-        }
-        
-      </View>
-      
-      </ScrollView>
-      <FlatList
-           data={listaFrutas}
-           renderItem={({item}) => <Text variant="displaySmall">{item}</Text>}         
-        />
-        <FlatList
-          data={listaFrutas}
-          renderItem={({item}) =>(
-            <View>
-              <Text variant="displaySmall">{item}</Text>
-              <Divider style={{}}/>
-            </View>
-          )
-        }
-        />
+     <Text variant="displayMedium">Lista de Estados</Text>
+     <FlatList
+     data={listaEstadosMunicipios}
+     renderItem={({item}) =>(
+      <Estados
+        nome ={item.nome}
+        sigla = {item.sigla}
+        descricao = {item.descricao}
+        imagem = {item.imagem}
+        municipios={item.municipios}
+      />   
+     )} 
+     />
+     </View>
     </PaperProvider>
   );
 }
